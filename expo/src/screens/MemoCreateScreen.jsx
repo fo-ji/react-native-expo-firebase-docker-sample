a8e90d5516b4cd87382bdcbe44a4bd2ac45d85b5
fo-ji <fumiya1753@gmail.com>
2022-09-29 17:36:29 +0900

    add KeyboardSafeView

--- expo/src/screens/MemoCreateScreen.jsx
+++ expo/src/screens/MemoCreateScreen.jsx
@@ -4,15 +4,18 @@ import { View, TextInput, StyleSheet, KeyboardAvoidingView } from 'react-native'
 
 import AppBar from '../components/AppBar'
 import CircleButton from '../components/CircleButton'
+// import KeyboardSafeView from '../components/KeyboardSafeView'
 
 export default function MemoCreateScreen() {
   return (
     <KeyboardAvoidingView behavior="height" style={styles.container}>
+      {/* <KeyboardSafeView style={styles.container}> */}
       <AppBar />
       <View style={styles.inputContainer}>
         <TextInput value="" multiline style={styles.input} />
       </View>
       <CircleButton name="check" />
+      {/* </KeyboardSafeView> */}
     </KeyboardAvoidingView>
   )
 }
